f075ff9ae1081a41c067ba98c8d3e2849cecfbec
leimu_hue <2627492139@qq.com>
2023-05-13 13:57:41 +0800

    提交更新

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using log4net;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace IntelligentControl.Views
 {
@@ -16,5 +19,40 @@ namespace IntelligentControl.Views
             logger.Info("main window was init");
             InitializeComponent();
         }
+
+        private void My_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            Point point = Mouse.GetPosition(sender as Grid);
+            if (point.Y <= 15)
+            {
+                this.DragMove();
+            }
+        }
+
+
+        private void LabelClick(object sender, RoutedEventArgs e) {
+            if (sender is null) {
+                return;
+            }
+            var label = sender as Label;
+            if (label is null) {
+                return;
+            }
+            label.Tag = 1;
+            var parent = label.Parent;
+            var count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++) {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (object.ReferenceEquals(child, label)) {
+                    continue;
+                }
+                if (child is Label tempLabel)
+                {
+                    tempLabel.Tag = -1;
+                }
+
+            }
+        }
+
     }
 }
